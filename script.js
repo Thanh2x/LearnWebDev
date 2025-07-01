eeadd4e34ac7ca0cd88ab3fad6b954664733d0b6
Thanh2x <tvhoang@ucsd.edu>
2025-07-01 04:43:57 -0700

    Day 5 progress

---
+++ script.js
@@ -0,0 +1,10 @@
+let bestNamingConvention = "CamelCase";
+const myAge = 21;
+var isHungry = true;
+let favQuote = 'She said "go again"';
+
+console.log(typeof (1 - "NaN"));
+
+console.log(favQuote[0]);
+console.log(favQuote.length);
+console.log(bestNamingConvention + " " + favQuote);
